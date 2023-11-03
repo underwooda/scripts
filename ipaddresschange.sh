ae70a37d96bb2e35b3864095563f3c5164845a04
underwooda <autech@fastmail.com>
2023-11-03 23:23:15 +0100

    script adjustment

--- ipaddresschange.sh
+++ ipaddresschange.sh
@@ -13,6 +13,7 @@ previous_public_ip=""
 
 #Domain Variable
 domain=
+
 # Function to send a message to the Telegram channel or group
 send_telegram_message() {
     local message="$1"
@@ -21,21 +22,27 @@ send_telegram_message() {
 
 # Function to resolve the CNAME and get the IP address
 resolve_cname() {
-    cname_ip=$(dig +short $domain)
+    cname_ip=$(dig +short +noquestion $domain)
     echo "$cname_ip"
 }
 
 # Function to get the public IP address
 get_public_ip() {
-    public_ip=$(curl -s ifconfig.me)
+    public_ip=$(curl -s -4 icanhazip.com)
     echo "$public_ip"
+}
+
+# Function to get the current timestamp
+get_timestamp() {
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "$timestamp"
 }
 
 # Initial resolves to set the previous IPs
 previous_cname_ip=$(resolve_cname)
 previous_public_ip=$(get_public_ip)
-echo "Initial CNAME IP: $previous_cname_ip" >> "$LOG_FILE"
-echo "Initial Public IP: $previous_public_ip" >> "$LOG_FILE"
+echo "$(get_timestamp) - Initial CNAME IP: $previous_cname_ip" >> "$LOG_FILE"
+echo "$(get_timestamp) - Initial Public IP: $previous_public_ip" >> "$LOG_FILE"
 
 # Continuously monitor for IP changes
 while true; do
@@ -43,7 +50,7 @@ while true; do
     current_public_ip=$(get_public_ip)
 
     if [ "$current_cname_ip" != "$previous_cname_ip" ]; then
-        timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+        timestamp=$(get_timestamp)
         cname_message="CNAME IP changed from $previous_cname_ip to $current_cname_ip at $timestamp for $domain"
         echo "$cname_message" >> "$LOG_FILE"
         send_telegram_message "$cname_message"
@@ -51,7 +58,7 @@ while true; do
     fi
 
     if [ "$current_public_ip" != "$previous_public_ip" ]; then
-        timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+        timestamp=$(get_timestamp)
         public_ip_message="Public IP changed from $previous_public_ip to $current_public_ip at $timestamp"
         echo "$public_ip_message" >> "$LOG_FILE"
         send_telegram_message "$public_ip_message"
